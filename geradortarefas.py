ced63e287ee9104bc505d6c06a19dc2d33515523
Henrique <henriqueaguiar670@gmail.com>
2025-06-05 21:14:38 -0300

    a

--- geradortarefas.py
+++ geradortarefas.py
@@ -10,7 +10,7 @@ class PassGen:
             [sg.Text('Site/Software', size=(10,1)),
             sg.Input(key='site', size=(20,1))],
             [sg.Text('E-mail/Usuário', size=(10,1)),
-            sg.Input(key='usuário', size=(20,1)),]
+            sg.Input(key='usuário', size=(20,1))],
             [sg.Text('Quantidade de caracteres'),
             sg.Combo(values=list(range(30)), key='total_chars', default_value=1, size=(3, 1))],
             [sg.Output(size=(32, 5))],
